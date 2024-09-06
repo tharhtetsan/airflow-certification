3548d1cb2214da0ccab8ceaae147ffbf169cffa8
Thar Htet San <tharhtet1234@gmail.com>
2024-09-06 23:11:28 +0700

    add

--- book/dags/v6_define_dag_obj_2_taskflow.py
+++ book/dags/v6_define_dag_obj_2_taskflow.py
@@ -1,24 +1,24 @@
 from airflow.decorators import dag,task
 from datetime import datetime
 
-from airflow.operators.python import PythonOperator
 
-@dag("v6_define_dag_obj_2_decorators", start_date=datetime(2024,8,12), 
+@dag("v6_define_dag_obj_2_taskflow", start_date=datetime(2024,8,12), 
         description="This is a hello world pipeline", tags=["hello"],
-        schedule='@daily',catchup=False )
+        schedule='@daily',catchup=False)
 
-def parent_task_flow():
+def parent_task():
     @task
     def print_a():
         print("hello from task a")
-
+        return 9999
 
     @task
-    def print_b():
+    def print_b(temp):
         print("hello from task b")
+        print(temp)
+    
+    temp = print_a()
+    print_b(temp)
 
-    print_a()
 
-    print_b()
-
-parent_task_flow_obj = parent_task_flow()
+parent_task()

65fca542e446b4215d81bed73fe60de48003ccbc
Thar Htet San <tharhtet1234@gmail.com>
2024-09-11 23:25:06 +0700

    add scheduler

--- book/dags/v8_airflow_schedule_3.py
+++ book/dags/v8_airflow_schedule_3.py
@@ -1,19 +1,23 @@
 from airflow import DAG
 from datetime import datetime
-
+from datetime import timedelta
+import time 
 from airflow.operators.python import PythonOperator
 
 def print_a():
+    time.sleep(3)
     print("hello from task a")
 
 def print_b():
+    time.sleep(1)
     print("hello from task b")
 
 
-with DAG("v4_helloworld", start_date=datetime(2024,8,12), 
+with DAG("v8_airflow_scheduler_3", start_date=datetime(2024,9,10), 
         description="This is a hello world pipeline", tags=["hello"],
-        schedule='@daily',catchup=False ):
+        schedule=timedelta(minutes=2),catchup=True ):
 
     task_a = PythonOperator(task_id="task_a", python_callable=print_a)
     task_b = PythonOperator(task_id="task_b", python_callable=print_b)
     
+task_a >> task_b
